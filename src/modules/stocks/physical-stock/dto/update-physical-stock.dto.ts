41a6476efcc3a30549e9907935bf5a8188067cc0
Viji <vksoftwaresnkl@gmail.com>
2026-05-05 11:33:55 +0530

    feat: implement physical stock module with controller, service, and DTOs

---
+++ src/modules/stocks/physical-stock/dto/update-physical-stock.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreatePhysicalStockDto } from './create-physical-stock.dto';
+
+export class UpdatePhysicalStockDto extends PartialType(CreatePhysicalStockDto) {}
---
+++ src/modules/stocks/physical-stock/physical-stock.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PhysicalStockController } from './physical-stock.controller';
+import { PhysicalStockService } from './physical-stock.service';
+
+describe('PhysicalStockController', () => {
+  let controller: PhysicalStockController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PhysicalStockController],
+      providers: [PhysicalStockService],
+    }).compile();
+
+    controller = module.get<PhysicalStockController>(PhysicalStockController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
