50a5e0a17250980ca4f53aba549ba97968fbe0ee
Viji <vksoftwaresnkl@gmail.com>
2026-05-08 18:32:14 +0530

    removed phy folder

--- src/modules/stocks/physical-stock/physical-stock.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PhysicalStockService } from './physical-stock.service';
-import { PhysicalStockController } from './physical-stock.controller';
-
-@Module({
-  controllers: [PhysicalStockController],
-  providers: [PhysicalStockService],
-})
-export class PhysicalStockModule {}
