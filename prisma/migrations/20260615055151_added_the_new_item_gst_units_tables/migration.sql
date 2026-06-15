9a4581bc235e15deff5af46986f7da2df41ec632
viji karthi <vksoftwaresnkl@gmail.com>
2026-06-15 11:29:03 +0530

    feat: add ItemGstUnits table to inventory schema and database migration

---
+++ prisma/migrations/20260615055151_added_the_new_item_gst_units_tables/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "inventory"."ItemGstUnits" (
+    "item_gst_unit_id" SERIAL NOT NULL,
+    "item_gst_unit_code" TEXT,
+    "item_gst_unit_name" TEXT,
+    "item_gst_unit_created_on" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "item_gst_unit_created_by" TEXT,
+    "item_gst_unit_modified_on" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "item_gst_unit_modified_by" TEXT,
+    "item_gst_unit_sync_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ItemGstUnits_pkey" PRIMARY KEY ("item_gst_unit_id")
+);
